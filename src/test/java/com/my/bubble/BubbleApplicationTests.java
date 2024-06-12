9c212dfe785a20f0bf9a99d0ae514e3a8710203c
jhj <jhhh.dev@gmail.com>
2024-06-12 10:17:40 +0900

    first commit

---
+++ src/test/java/com/my/bubble/BubbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.bubble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BubbleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
